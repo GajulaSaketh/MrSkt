d832fe8f12e96667b6f1c3fa13a0ed06e2ec23ae
GajulaSaketh <sakethgajula19@gmail.com>
2025-05-01 12:58:36 +0530

    hashrouting

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter  as Router, Routes, Route } from "react-router-dom";
 import './App.css';
 import MobileUi from './components/MobileUi';
 import WebApp from './components/WebApp';
