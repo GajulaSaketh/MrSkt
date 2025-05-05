354dd32d940bd18ceb5aa23206053e673a46db2b
GajulaSaketh <sakethgajula19@gmail.com>
2025-05-05 09:43:08 +0530

    Removed all effect from blog component

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -3,36 +3,10 @@ import "../styles/Blog.css";
 
 
 const Blog = () => {
-    const [showGlitch, setShowGlitch] = useState(false);
-
-  useEffect(() => {
-    const glitchShown = sessionStorage.getItem('glitchShown');
-    if (!glitchShown) {
-        setShowGlitch(true);
-  
-        const audio = new Audio('/sounds/glitch.mp3');
-        audio.volume = 0.4;
-        audio.play();
-  
-        // Mark glitch as shown in sessionStorage
-        sessionStorage.setItem('glitchShown', 'true');
-  
-        const timer = setTimeout(() => setShowGlitch(false), 3000);
-        return () => clearTimeout(timer);
-      }
-  }, []);
+    
 
   return (
     <section className="blog-section" id="blog">
-        {showGlitch && (
-        <div className="glitch-overlay">
-          <img
-            src="https://media.tenor.com/Mw__tI1Xe1YAAAAC/glitch-error.gif"
-            alt="Glitch"
-          />
-        </div>
-      )}
-    <div className="blog-content">
     <div className="gallery">
       <div className="grid-item large">
         <img src="https://via.placeholder.com/300x200" alt="Image 1" />
@@ -61,7 +35,6 @@ const Blog = () => {
       <div className="grid-item medium">
         <img src="https://via.placeholder.com/300x200" alt="Image 9" />
       </div>
-    </div>
     </div>
     </section>
   );
