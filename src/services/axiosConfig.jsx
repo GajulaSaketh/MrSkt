0b958295fc3e98164d42d6b6016f6faceb1f9600
GajulaSaketh <sakethgajula19@gmail.com>
2025-05-20 12:30:34 +0530

    replaced the baseurl from json to axios file

--- src/services/axiosConfig.jsx
+++ src/services/axiosConfig.jsx
@@ -2,12 +2,11 @@ import axios from 'axios';
 
 export const useAxiosInstance = () => {
   const instance = axios.create({
-    baseURL: '/',
+    baseURL: 'https://mrtech-bgdhf9d2ekhadrgh.canadacentral-01.azurewebsites.net',
     timeout: 10000, 
     headers: {
       'Content-Type': 'application/json'
     }
   });
-
   return instance;
 };
